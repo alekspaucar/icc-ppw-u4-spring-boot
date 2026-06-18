d5c13f524b3fbc551ec19e27e362231b4f5b16b1
Erick Paucar <erickvzh@MacBook-Pro-de-Erick.local>
2026-06-18 10:44:54 -0500

    StudentController

--- src/main/java/ec/edu/ups/icc/fundamentos01/students/controllers/StudentController.java
+++ src/main/java/ec/edu/ups/icc/fundamentos01/students/controllers/StudentController.java
@@ -1,5 +1,26 @@
 package ec.edu.ups.icc.fundamentos01.students.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ec.edu.ups.icc.fundamentos01.students.models.Student;
+
+@RestController
+@RequestMapping("/students")
 public class StudentController {
-    
+    private List<Student> students = new ArrayList<>();
+
+    public StudentController() {
+        students.add(new Student(2, "Juan", "30"));
+        students.add(new Student(1, "Diego", "10"));
+    }
+
+    @GetMapping()
+    public List<Student> getStudents() {
+        return students;
+    }
 }
